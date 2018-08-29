669919c98c1fdf3018beba27e5d28cc8ac89a5d3
Harsh Gupta <harshgupt91@gmail.com>
2018-08-29 18:32:55 +0800

    First commit

---
+++ Adventure Game/Assets/Script/Blade.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+    public GameObject prefabBladeTrail;
+
+    Camera cam;
+    GameObject currentBladeTrail;
+    Rigidbody2D rb;
+    CircleCollider2D circleCollider;
+    Vector2 previousPosition;
+    Vector2 touchPosition;
+    bool isCutting = false;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void Update ()
+    {
+#if !UNITY_EDITOR
+        Touch touch = Input.touches[0];
+        if(touch.phase == TouchPhase.Began)
+        {
+            touchPosition = touch.position;
+            StartCutting();
+        }
+        else if(touch.phase == TouchPhase.Moved)
+        {
+            touchPosition = touch.position;
+        }
+        else if(touch.phase == TouchPhase.Ended)
+        {
+            StopCutting();
+        }
+#else
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchPosition = Input.mousePosition;
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+#endif
+        if (isCutting)
+        {
+            UpdateBlade();
+        }
+	}
+
+    public void UpdateBlade()
+    {
+#if !UNITY_EDITOR
+        Vector2 newPosition = cam.ScreenToWorldPoint(touchPosition);
+#else
+        Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+#endif
+        rb.position = newPosition;
+        previousPosition = newPosition;
+    }
+
+    public void StartCutting()
+    {
+        isCutting = true;
+        rb.position = cam.ScreenToWorldPoint(touchPosition);
+        transform.position = rb.position;
+        currentBladeTrail = Instantiate(prefabBladeTrail, transform);
+        previousPosition = cam.ScreenToWorldPoint(touchPosition);
+        circleCollider.enabled = true;
+    }
+
+    public void StopCutting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 0.5f);
+        circleCollider.enabled = false;
+    }
+}
---
+++ Adventure Game/Assets/Script/ObjectScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaler : MonoBehaviour {
+
+    public float height;
+    public float width;
+    public float ratio;
+
+    void Start()
+    {
+        height = Screen.height;
+        width = Screen.width;
+        ratio = height / width;
+        //transform.localScale = transform.localScale * (ratio) / 2;
+        Camera.main.orthographicSize = 7.64f / (ratio * 1.5f);
+        Debug.Log(height + " " + width + " " + ratio);
+    }
+}
