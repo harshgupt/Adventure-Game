f3de6da24fd30a92371328a083ba04846f537b43
Harsh Gupta <harshgupt91@gmail.com>
2018-09-04 14:10:10 +0800

    Added scrollable Inventory with 30 slots

--- Adventure Game/Assets/Script/MiningScript.cs
+++ Adventure Game/Assets/Script/MiningScript.cs
@@ -7,14 +7,37 @@ public class MiningScript : MonoBehaviour {
 
     public Text amountText;
 
-    int amount = 0;
+    private void Start()
+    {
+        
+    }
 
     public void OnMouseDown()
     {
-        if(gameObject.tag == "Ore")
+        if(gameObject.tag == "Copper Ore")
         {
-            amount++;
-            amountText.text = amount.ToString();
+            Inventory.copperOre++;
+            amountText.text = Inventory.copperOre.ToString();
+        }
+        else if (gameObject.tag == "Tin Ore")
+        {
+            Inventory.tinOre++;
+            amountText.text = Inventory.tinOre.ToString();
+        }
+        else if (gameObject.tag == "Iron Ore")
+        {
+            Inventory.ironOre++;
+            amountText.text = Inventory.ironOre.ToString();
+        }
+        else if (gameObject.tag == "Coal Ore")
+        {
+            Inventory.coalOre++;
+            amountText.text = Inventory.coalOre.ToString();
+        }
+        else if (gameObject.tag == "Lead Ore")
+        {
+            Inventory.leadOre++;
+            amountText.text = Inventory.leadOre.ToString();
         }
     }
 }
---
+++ Adventure Game/Assets/Script/Smelting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Smelting : MonoBehaviour {
+
+    public Text copperOreAmount;
+    public Text tinOreAmount;
+    public Text ironOreAmount;
+    public Text coalOreAmount;
+    public Text leadOreAmount;
+
+    private void Start()
+    {
+        copperOreAmount.text = Inventory.copperOre.ToString();
+        tinOreAmount.text = Inventory.tinOre.ToString();
+        ironOreAmount.text = Inventory.ironOre.ToString();
+        coalOreAmount.text = Inventory.coalOre.ToString();
+        leadOreAmount.text = Inventory.leadOre.ToString();
+    }
+}
