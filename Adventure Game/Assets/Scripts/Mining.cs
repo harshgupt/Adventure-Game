6cd86173b9a2758d24b9d3c0e265d819d9515d7d
Harsh Gupta <harshgupt91@gmail.com>
2018-10-04 18:00:46 +0800

    Mining fixed

--- Adventure Game/Assets/Scripts/Mining.cs
+++ Adventure Game/Assets/Scripts/Mining.cs
@@ -5,60 +5,164 @@ using UnityEngine.UI;
 
 public class Mining : MonoBehaviour {
     
-    public InventoryScript inventoryManagerScript;
     public Item[] ores = new Item[9];
+
+    public GameObject copperOre;
+    public GameObject tinOre;
+    public GameObject ironOre;
+    public GameObject coalOre;
+    public GameObject leadOre;
+    public GameObject mithrilOre;
+    public GameObject adamantineOre;
+    public GameObject quicksilverOre;
+    public GameObject meteoriteOre;
+
+    public int currentOre = 0;
 
     public void CopperMine()
     {
         PlayerData.ores[0]++;
-        inventoryManagerScript.AddItem("Copper Ore", ores[0].sprite, PlayerData.ores[0]);
     }
 
     public void TinMine()
     {
         PlayerData.ores[1]++;
-        inventoryManagerScript.AddItem("Tin Ore", ores[1].sprite, PlayerData.ores[1]);
     }
 
     public void IronMine()
     {
         PlayerData.ores[2]++;
-        inventoryManagerScript.AddItem("Iron Ore", ores[2].sprite, PlayerData.ores[2]);
     }
 
     public void CoalMine()
     {
         PlayerData.ores[3]++;
-        inventoryManagerScript.AddItem("Coal Ore", ores[3].sprite, PlayerData.ores[3]);
     }
 
     public void LeadMine()
     {
         PlayerData.ores[4]++;
-        inventoryManagerScript.AddItem("Lead Ore", ores[4].sprite, PlayerData.ores[4]);
     }
 
     public void MithrilMine()
     {
         PlayerData.ores[5]++;
-        inventoryManagerScript.AddItem("Mithril Ore", ores[5].sprite, PlayerData.ores[5]);
     }
 
-    public void AdamantiumMine()
+    public void AdamantineMine()
     {
         PlayerData.ores[6]++;
-        inventoryManagerScript.AddItem("Adamantium Ore", ores[6].sprite, PlayerData.ores[6]);
     }
 
     public void QuicksilverMine()
     {
         PlayerData.ores[7]++;
-        inventoryManagerScript.AddItem("Quicksilver Ore", ores[7].sprite, PlayerData.ores[7]);
     }
 
     public void MeteoriteMine()
     {
         PlayerData.ores[8]++;
-        inventoryManagerScript.AddItem("Meteorite Ore", ores[8].sprite, PlayerData.ores[8]);
+    }
+
+    public void UpButton()
+    {
+        switch (currentOre)
+        {
+            case 0:
+                return;
+            case 1:
+                copperOre.SetActive(true);
+                tinOre.SetActive(false);
+                currentOre--;
+                break;
+            case 2:
+                tinOre.SetActive(true);
+                ironOre.SetActive(false);
+                currentOre--;
+                break;
+            case 3:
+                ironOre.SetActive(true);
+                coalOre.SetActive(false);
+                currentOre--;
+                break;
+            case 4:
+                coalOre.SetActive(true);
+                leadOre.SetActive(false);
+                currentOre--;
+                break;
+            case 5:
+                leadOre.SetActive(true);
+                mithrilOre.SetActive(false);
+                currentOre--;
+                break;
+            case 6:
+                mithrilOre.SetActive(true);
+                adamantineOre.SetActive(false);
+                currentOre--;
+                break;
+            case 7:
+                adamantineOre.SetActive(true);
+                quicksilverOre.SetActive(false);
+                currentOre--;
+                break;
+            case 8:
+                quicksilverOre.SetActive(true);
+                meteoriteOre.SetActive(false);
+                currentOre--;
+                break;
+            default:
+                return;
+        }
+    }
+
+    public void DownButton()
+    {
+        switch (currentOre)
+        {
+            case 0:
+                copperOre.SetActive(false);
+                tinOre.SetActive(true);
+                currentOre++;
+                break;
+            case 1:
+                tinOre.SetActive(false);
+                ironOre.SetActive(true);
+                currentOre++;
+                break;
+            case 2:
+                ironOre.SetActive(false);
+                coalOre.SetActive(true);
+                currentOre++;
+                break;
+            case 3:
+                coalOre.SetActive(false);
+                leadOre.SetActive(true);
+                currentOre++;
+                break;
+            case 4:
+                leadOre.SetActive(false);
+                mithrilOre.SetActive(true);
+                currentOre++;
+                break;
+            case 5:
+                mithrilOre.SetActive(false);
+                adamantineOre.SetActive(true);
+                currentOre++;
+                break;
+            case 6:
+                adamantineOre.SetActive(false);
+                quicksilverOre.SetActive(true);
+                currentOre++;
+                break;
+            case 7:
+                quicksilverOre.SetActive(false);
+                meteoriteOre.SetActive(true);
+                currentOre++;
+                break;
+            case 8:
+                return;
+            default:
+                return;
+        }
     }
 }
