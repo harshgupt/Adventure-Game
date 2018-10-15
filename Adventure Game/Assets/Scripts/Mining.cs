f24ba313bcb70ab385848128ef6c9ae1d4cc0a13
Harsh Gupta <harshgupt91@gmail.com>
2018-10-15 15:28:06 +0800

    Mining of new ores added, along with sprites

--- Adventure Game/Assets/Scripts/Mining.cs
+++ Adventure Game/Assets/Scripts/Mining.cs
@@ -5,17 +5,27 @@ using UnityEngine.UI;
 
 public class Mining : MonoBehaviour {
     
-    public Item[] ores = new Item[9];
+    public Sprite[] oreSprites = new Sprite[DataManager.numOres];
 
     public GameObject copperOre;
     public GameObject tinOre;
     public GameObject ironOre;
     public GameObject coalOre;
+    public GameObject aluminiumOre;
+    public GameObject nickelOre;
+    public GameObject magnesiumOre;
+    public GameObject zincOre;
     public GameObject leadOre;
-    public GameObject mithrilOre;
-    public GameObject adamantineOre;
-    public GameObject quicksilverOre;
+    public GameObject silverOre;
+    public GameObject goldOre;
+    public GameObject platinumOre;
+    public GameObject obsidianOre;
     public GameObject meteoriteOre;
+    public GameObject mithrilOre;
+    public GameObject adamanteusOre;
+    public GameObject quicksilverOre;
+    public GameObject aetherOre;
+    public GameObject crimsoniteOre;
 
     public Image inventoryImage;
 
@@ -28,54 +38,14 @@ public class Mining : MonoBehaviour {
         DisplayInventoryAmount();
     }
 
-    public void CopperMine()
+    public void MineOre()
     {
-        PlayerData.ores[0]++;
-    }
-
-    public void TinMine()
-    {
-        PlayerData.ores[1]++;
-    }
-
-    public void IronMine()
-    {
-        PlayerData.ores[2]++;
-    }
-
-    public void LeadMine()
-    {
-        PlayerData.ores[3]++;
-    }
-
-    public void CoalMine()
-    {
-        PlayerData.ores[4]++;
-    }
-
-    public void MithrilMine()
-    {
-        PlayerData.ores[5]++;
-    }
-
-    public void AdamantineMine()
-    {
-        PlayerData.ores[6]++;
-    }
-
-    public void QuicksilverMine()
-    {
-        PlayerData.ores[7]++;
-    }
-
-    public void MeteoriteMine()
-    {
-        PlayerData.ores[8]++;
+        PlayerData.ores[currentOre]++;
     }
 
     public void DisplayInventoryAmount()
     {
-        inventoryImage.sprite = ores[currentOre].sprite;
+        inventoryImage.sprite = oreSprites[currentOre];
         inventoryAmount.text = PlayerData.ores[currentOre].ToString();
     }
 
@@ -97,32 +67,82 @@ public class Mining : MonoBehaviour {
                 break;
             case 3:
                 ironOre.SetActive(true);
-                leadOre.SetActive(false);
-                currentOre--;
-                break;
-            case 4:
-                leadOre.SetActive(true);
                 coalOre.SetActive(false);
                 currentOre--;
                 break;
-            case 5:
+            case 4:
                 coalOre.SetActive(true);
-                mithrilOre.SetActive(false);
+                aluminiumOre.SetActive(false);
+                currentOre--;
+                break;
+            case 5:
+                aluminiumOre.SetActive(true);
+                nickelOre.SetActive(false);
                 currentOre--;
                 break;
             case 6:
-                mithrilOre.SetActive(true);
-                adamantineOre.SetActive(false);
+                nickelOre.SetActive(true);
+                magnesiumOre.SetActive(false);
                 currentOre--;
                 break;
             case 7:
-                adamantineOre.SetActive(true);
-                quicksilverOre.SetActive(false);
+                magnesiumOre.SetActive(true);
+                zincOre.SetActive(false);
                 currentOre--;
                 break;
             case 8:
-                quicksilverOre.SetActive(true);
+                zincOre.SetActive(true);
+                leadOre.SetActive(false);
+                currentOre--;
+                break;
+            case 9:
+                leadOre.SetActive(true);
+                silverOre.SetActive(false);
+                currentOre--;
+                break;
+            case 10:
+                silverOre.SetActive(true);
+                goldOre.SetActive(false);
+                currentOre--;
+                break;
+            case 11:
+                goldOre.SetActive(true);
+                platinumOre.SetActive(false);
+                currentOre--;
+                break;
+            case 12:
+                platinumOre.SetActive(true);
+                obsidianOre.SetActive(false);
+                currentOre--;
+                break;
+            case 13:
+                obsidianOre.SetActive(true);
                 meteoriteOre.SetActive(false);
+                currentOre--;
+                break;
+            case 14:
+                meteoriteOre.SetActive(true);
+                mithrilOre.SetActive(false);
+                currentOre--;
+                break;
+            case 15:
+                mithrilOre.SetActive(true);
+                adamanteusOre.SetActive(false);
+                currentOre--;
+                break;
+            case 16:
+                adamanteusOre.SetActive(true);
+                quicksilverOre.SetActive(false);
+                currentOre--;
+                break;
+            case 17:
+                quicksilverOre.SetActive(true);
+                aetherOre.SetActive(false);
+                currentOre--;
+                break;
+            case 18:
+                aetherOre.SetActive(true);
+                crimsoniteOre.SetActive(false);
                 currentOre--;
                 break;
             default:
@@ -146,35 +166,85 @@ public class Mining : MonoBehaviour {
                 break;
             case 2:
                 ironOre.SetActive(false);
-                leadOre.SetActive(true);
-                currentOre++;
-                break;
-            case 3:
-                leadOre.SetActive(false);
                 coalOre.SetActive(true);
                 currentOre++;
                 break;
-            case 4:
+            case 3:
                 coalOre.SetActive(false);
-                mithrilOre.SetActive(true);
+                aluminiumOre.SetActive(true);
+                currentOre++;
+                break;
+            case 4:
+                aluminiumOre.SetActive(false);
+                nickelOre.SetActive(true);
                 currentOre++;
                 break;
             case 5:
-                mithrilOre.SetActive(false);
-                adamantineOre.SetActive(true);
+                nickelOre.SetActive(false);
+                magnesiumOre.SetActive(true);
                 currentOre++;
                 break;
             case 6:
-                adamantineOre.SetActive(false);
-                quicksilverOre.SetActive(true);
+                magnesiumOre.SetActive(false);
+                zincOre.SetActive(true);
                 currentOre++;
                 break;
             case 7:
-                quicksilverOre.SetActive(false);
-                meteoriteOre.SetActive(true);
+                zincOre.SetActive(false);
+                leadOre.SetActive(true);
                 currentOre++;
                 break;
             case 8:
+                leadOre.SetActive(false);
+                silverOre.SetActive(true);
+                currentOre++;
+                break;
+            case 9:
+                silverOre.SetActive(false);
+                goldOre.SetActive(true);
+                currentOre++;
+                break;
+            case 10:
+                goldOre.SetActive(false);
+                platinumOre.SetActive(true);
+                currentOre++;
+                break;
+            case 11:
+                platinumOre.SetActive(false);
+                obsidianOre.SetActive(true);
+                currentOre++;
+                break;
+            case 12:
+                obsidianOre.SetActive(false);
+                meteoriteOre.SetActive(true);
+                currentOre++;
+                break;
+            case 13:
+                meteoriteOre.SetActive(false);
+                mithrilOre.SetActive(true);
+                currentOre++;
+                break;
+            case 14:
+                mithrilOre.SetActive(false);
+                adamanteusOre.SetActive(true);
+                currentOre++;
+                break;
+            case 15:
+                adamanteusOre.SetActive(false);
+                quicksilverOre.SetActive(true);
+                currentOre++;
+                break;
+            case 16:
+                quicksilverOre.SetActive(false);
+                aetherOre.SetActive(true);
+                currentOre++;
+                break;
+            case 17:
+                aetherOre.SetActive(false);
+                crimsoniteOre.SetActive(true);
+                currentOre++;
+                break;
+            case 18:
                 return;
             default:
                 return;
