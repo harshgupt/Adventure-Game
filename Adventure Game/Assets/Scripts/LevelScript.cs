edbac016786eb6e0f6dd72383f39590b0fe400ec
Harsh Gupta <harshgupt91@gmail.com>
2018-10-22 16:30:50 +0800

    Deleted extra scenes

--- Adventure Game/Assets/Scripts/LevelScript.cs
+++ Adventure Game/Assets/Scripts/LevelScript.cs
@@ -9,34 +9,17 @@ public class LevelScript : MonoBehaviour {
     public static int level = 1;
     public static int wave = 1;
 
-    public static bool nextLevel = false;
-
     private void Start()
     {
         if(SceneManager.GetActiveScene().name == "Start")
         {
-            LoadLevel();
-        }
-    }
-
-    private void Update()
-    {
-        if(nextLevel)
-        {
-            nextLevel = false;
-            wave = 0;
-            level++;
-        }
-    }
-
-    public void LoadLevel()
-    {
-        if (File.Exists(Path.Combine(Application.streamingAssetsPath, "data.json")))
-        {
-            string data = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "data.json"));
-            PlayerDataJSON loadedData = JsonUtility.FromJson<PlayerDataJSON>(data);
-            level = loadedData.Level;
-            wave = loadedData.Wave;
+            //Load Level
+            if (File.Exists(Path.Combine(Application.streamingAssetsPath, "data.json")))
+            {
+                PlayerDataJSON loadedData = JsonUtility.FromJson<PlayerDataJSON>(File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "data.json")));
+                level = loadedData.Level;
+                wave = loadedData.Wave;
+            }
         }
     }
 
